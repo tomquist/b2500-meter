47a3299e9b961dfa12898f62aae38b712e215e7e
Tom Quist <tomquist@users.noreply.github.com>
2026-02-06 22:23:10 +0100

    fix: remove disable_absolute handling for ct002/ct003

--- main.py
+++ main.py
@@ -104,9 +104,6 @@ def run_device(
                 value1 = value1 + value2 + value3
                 value2 = value3 = 0
 
-            if not disable_absolute:
-                value1, value2, value3 = map(abs, (value1, value2, value3))
-
             device.value = [value1, value2, value3]
 
         device.before_send = update_readings
@@ -117,7 +114,6 @@ def run_device(
             if args.disable_sum is not None
             else cfg.getboolean("GENERAL", "DISABLE_SUM_PHASES", fallback=False)
         )
-        disable_absolute = True
 
         ct_section = "CT002"
         if device_type == "ct003" and cfg.has_section("CT003"):
@@ -175,9 +171,6 @@ def run_device(
             if not disable_sum:
                 value1 = value1 + value2 + value3
                 value2 = value3 = 0
-
-            if not disable_absolute:
-                value1, value2, value3 = map(abs, (value1, value2, value3))
 
             return [value1, value2, value3]
 
